fc93e54c75f8e8f8de890d4ca1c5a4210ad9cfb1
Didee <miss.sonagi@gmail.com>
2017-06-26 17:35:50 +0900

    Edit timestable

--- timestable.js
+++ timestable.js
@@ -1,22 +1,6 @@
-for(var k = 1;k<10;k++){
-	for(var l = 1; l < 10; l++){
-		console.log(k*l);
-	}
-}
+var square = (i => (j => j * i));
 
+for(var j = 2; j < 10; j++)
+	for(var k = 1; k < 10; k++)
+		console.log(square(k)(j));
 
-var number = function(x){
-	var result = function(){
-		for(var num = 1; num < 10; num++){
-			console.log(" >> " + x * num);
-			return x * num;
-		}
-	}
-	console.log(result);
-	return result;
-}
-
-for(var i = 1 ; i < 10 ; i++){
-	multiplier = number(i);
-	console.log(multiplier());
-}
