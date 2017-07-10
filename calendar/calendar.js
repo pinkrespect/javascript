03504f8989672d9b9bb899898fdbe46a1fbc3505
Didee <miss.sonagi@gmail.com>
2017-07-10 23:19:30 +0900

    Edit .js, .html, .css file indent.

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,40 +1,40 @@
 // Sets this month's last day, first day, today.
 function dateObject(){
-	var lastday, firstday;
-	if(arguments.length == 3){
-		if(arguments[1] != 12){
-			lastday = new Date(arguments[0], arguments[1]+1, 0);
-			firstday = new Date(arguments[0], arguments[1], 1);
-		}else{
-			lastday = new Date(arguments[0]+1, 0, 1);
-			firstday = new Date(arguments[0], arguments[1], 1);
-		}
-		dateText = firstday.toDateString().split(" "); 
+    var lastday, firstday;
+    if(arguments.length == 3){
+        if(arguments[1] != 12){
+            lastday = new Date(arguments[0], arguments[1]+1, 0);
+            firstday = new Date(arguments[0], arguments[1], 1);
+        }else{
+            lastday = new Date(arguments[0]+1, 0, 1);
+            firstday = new Date(arguments[0], arguments[1], 1);
+        }
+        dateText = firstday.toDateString().split(" "); 
 
-		return{
-			name: arguments[2],
-			year: firstday.getUTCFullYear(),
-			month: firstday.getMonth(),
-			monthText: dateText[1],
-			firstDate: firstday.getDate(),
-			lastDate: lastday.getDate(),
-			firstDay: firstday.getDay(),
-			lastDay: lastday.getDay()
-		}
+        return{
+            name: arguments[2],
+            year: firstday.getUTCFullYear(),
+            month: firstday.getMonth(),
+            monthText: dateText[1],
+            firstDate: firstday.getDate(),
+            lastDate: lastday.getDate(),
+            firstDay: firstday.getDay(),
+            lastDay: lastday.getDay()
+        }
 
-	} else{
-		this.date = new Date();
-		dateText = this.date.toDateString().split(" ");
+    } else{
+        this.date = new Date();
+        dateText = this.date.toDateString().split(" ");
 
-		return {
-			name: arguments[0],
-			year: this.date.getUTCFullYear(),
-			month: this.date.getMonth(),
-			monthText: dateText[1],
-			date: this.date.getDate(),
-			day: this.date.getDay()
-		}
-	}
+        return {
+            name: arguments[0],
+            year: this.date.getUTCFullYear(),
+            month: this.date.getMonth(),
+            monthText: dateText[1],
+            date: this.date.getDate(),
+            day: this.date.getDay()
+        }
+    }
 }
 
 const now = dateObject("now");
@@ -47,68 +47,68 @@ const weeks = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 // Sets maximum date for a month.
 const dates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-	11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-	21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 function drawCalendar(daysObject){
-	// Make date iterator. 
-	var iter = function* () {
-		var index = 0;
-		while(dates[index] <= daysObject.lastDate){
-			yield dates[index++];
-		}
-	}
-	var iterator = iter();
-	// Set component's id, attribute.
-	const date = document.createElement("p");
-	date.id = daysObject.name;
+    // Make date iterator. 
+    var iter = function* () {
+        var index = 0;
+        while(dates[index] <= daysObject.lastDate){
+            yield dates[index++];
+        }
+    }
+    var iterator = iter();
+    // Set component's id, attribute.
+    const date = document.createElement("p");
+    date.id = daysObject.name;
 
-	const calendarTitle = document.createElement("div");
-	calendarTitle.id = daysObject.name + "Title";
+    const calendarTitle = document.createElement("div");
+    calendarTitle.id = daysObject.name + "Title";
 
-	const daysTable = document.createElement("table");
-	daysTable.id = daysObject.name + "Days";
+    const daysTable = document.createElement("table");
+    daysTable.id = daysObject.name + "Days";
 
-	const dateTable = document.createElement("table");
-	dateTable.id = daysObject.name + "Date";
+    const dateTable = document.createElement("table");
+    dateTable.id = daysObject.name + "Date";
 
-	// Make titleLine content.
-	calendarTitle.innerHTML = "<strong>" + daysObject.monthText + "</strong>";
+    // Make titleLine content.
+    calendarTitle.innerHTML = "<strong>" + daysObject.monthText + "</strong>";
 
-	// Make daysTable contents.
-	var daysRow = daysTable.insertRow();
-	for(x of weeks){
-		daysRow.insertCell().innerHTML = "<strong>" + x + "</strong>";
-	}
+    // Make daysTable contents.
+    var daysRow = daysTable.insertRow();
+    for(x of weeks){
+        daysRow.insertCell().innerHTML = "<strong>" + x + "</strong>";
+    }
 
-	// Make dateTable contents. 
-	for(var i = 0; i <= 5;i++){
-		var dateRow = dateTable.insertRow();
-		dateRow.setAttribute("style", "height:25px;");
-		for(var j=0; j < 7;j++){
-			if(i == 0 && j < daysObject.firstDay){
-				dateRow.insertCell().innerHTML = "";
-			}else{
-				var number = iterator.next().value;
-				if(number != undefined && number <= daysObject.lastDate)
-					if(daysObject.name == "thisMonth" && number == now.date){
-						// Make today's date bold.
-						var todateCell = dateRow.insertCell();
-						todateCell.id = "todateCell";
-						todateCell.innerHTML = "<strong>" + number + "</strong>";
-					} else
-					dateRow.insertCell().innerHTML = number; 
-				else
-					dateRow.insertCell().innerHTML = "";
-			}
-		}
-	}
+    // Make dateTable contents. 
+    for(var i = 0; i <= 5;i++){
+        var dateRow = dateTable.insertRow();
+        dateRow.setAttribute("style", "height:25px;");
+        for(var j=0; j < 7;j++){
+            if(i == 0 && j < daysObject.firstDay){
+                dateRow.insertCell().innerHTML = "";
+            }else{
+                var number = iterator.next().value;
+                if(number != undefined && number <= daysObject.lastDate)
+                    if(daysObject.name == "thisMonth" && number == now.date){
+                        // Make today's date bold.
+                        var todateCell = dateRow.insertCell();
+                        todateCell.id = "todateCell";
+                        todateCell.innerHTML = "<strong>" + number + "</strong>";
+                    } else
+                    dateRow.insertCell().innerHTML = number; 
+                else
+                    dateRow.insertCell().innerHTML = "";
+            }
+        }
+    }
 
-	// Append Child Node to each Parent Node.
-	date.appendChild(calendarTitle);
-	date.appendChild(daysTable);
-	date.appendChild(dateTable);
-	return date;
+    // Append Child Node to each Parent Node.
+    date.appendChild(calendarTitle);
+    date.appendChild(daysTable);
+    date.appendChild(dateTable);
+    return date;
 }
 
 // Set title to today's date.
